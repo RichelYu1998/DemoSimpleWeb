9978a2f5ea54d58b36b8fac7d982ef12f5dd8143
余浩 <980187223@qq.com>
2020-06-15 11:41:18 +0800

    添加Serlvet

---
+++ src/main/java/cn/tedu/Servlet/Servlet.java
@@ -0,0 +1,14 @@
+package cn.tedu.Servlet;
+
+import java.io.IOException;
+
+@javax.servlet.annotation.WebServlet(name = "Servlet", urlPatterns = "/Servlet")
+public class Servlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
